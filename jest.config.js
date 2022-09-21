477463ffadc5a461ecbea6bbf31a425eb541442c
Jeong-seungok <vsnm25@naver.com>
2022-09-21 17:01:10 +0900

    Add: coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.jest.json" }],
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
